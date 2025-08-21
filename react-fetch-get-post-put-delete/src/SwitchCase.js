c7509222ef4327a9ba6251327c1623dfac95dffd
Abhishek Sardar <143304955+Abhishek-sard@users.noreply.github.com>
2025-08-21 08:04:36 +0545

    push

---
+++ react-fetch-get-post-put-delete/src/SwitchCase.js
@@ -0,0 +1,29 @@
+let dayOfWeek = new Date().getDay();
+let message;
+
+switch(dayOfWeek){
+    case 0:
+        message = "Sunday";
+        break;
+    case 1:
+        message = "Monday";
+        break;
+    case 2:
+        message = "Tuesday";
+        break;
+    case 3:
+        message = "Wednesday";
+        break;
+    case 4:
+        message = "Thursday";
+        break;
+    case 5:
+        message = "Friday";
+        break;
+    case 6:
+        message = "Saturday";
+        break;
+    default:
+        message = "Unknown";
+}
+console.log(`Today is: ${message}`);
